ca0c20ff615f4488b9544892bd15e400cb05c6ce
Amanite <until.the.end.of.the.universe@gmail.com>
2014-12-17 17:05:00 +0100

    Update dots.ml

--- dots.ml
+++ dots.ml
@@ -69,7 +69,7 @@ let draw_random_dots (x,y) =
 
 let usage() =
 	Printf.printf "Genetic algorithm building pictures based on evolutive pressure and random number generation \n
-	Syntax : %s <file>, where file is a valid picture file, format supported : .png, .jpg, .bmp, .gif, .ppm, .pgm" Sys.argv.(0)
+	Syntax : %s <file>, where file is a valid picture file, format supported : .png, .jpg, .bmp, .gif, .ppm, .pgm " Sys.argv.(0)
 	
 let original = array_of_pic Sys.argv.(1)
 
